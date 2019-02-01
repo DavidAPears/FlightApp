48657e1cb0694bc9a4f0bff2292d8d14ba522698
David A Pears <davidapears@gmail.com>
2019-02-01 09:02:25 +0000

    FINAL COMMIT

--- src/App.js
+++ src/App.js
@@ -10,27 +10,30 @@ import banner from './styles/banner2.png';
 
 class App extends Component {
 
+// STATE(S)
   state = {
-    date: null,
-    query: '',
+    date            : null,
+    query           : '',
     departuresActive: false,
-    arrivalsActive: false,
+    arrivalsActive  : false,
     allFlightsActive: true,
-    curTime: null
+    curTime         : null
     };
 
+// CONSTRUCTOR(S)
   constructor(props) {
   super(props);
-  this.getDepartures = this.getDepartures.bind(this);
-  this.getArrivals = this.getArrivals.bind(this);
-  this.getDataFromDb = this.getDataFromDb.bind(this);
-  this.handleInputChange = this.handleInputChange.bind(this);
-  this.searchFlightNo = this.searchFlightNo.bind(this);
-  this.setDepartureTabActive = this.setDepartureTabActive.bind(this);
-  this.setArrivalsTabActive = this.setArrivalsTabActive.bind(this);
-  this.setAllFlightsTabActive = this.setAllFlightsTabActive.bind(this);
+  this.getDepartures            = this.getDepartures.bind(this);
+  this.getArrivals              = this.getArrivals.bind(this);
+  this.getDataFromDb            = this.getDataFromDb.bind(this);
+  this.handleInputChange        = this.handleInputChange.bind(this);
+  this.searchFlightNo           = this.searchFlightNo.bind(this);
+  this.setDepartureTabActive    = this.setDepartureTabActive.bind(this);
+  this.setArrivalsTabActive     = this.setArrivalsTabActive.bind(this);
+  this.setAllFlightsTabActive   = this.setAllFlightsTabActive.bind(this);
   }
 
+// COMPONENT DID MOUNT(S)
   componentDidMount() {
     this.getDataFromDb()
     setInterval( () => {
@@ -40,9 +43,7 @@ class App extends Component {
     },1000);
   }
 
-
-
-// Method that uses backend api to fetch data from our data base
+// METHOD TO GET DATA FROM BACKEND API
   getDataFromDb() {
     this.setAllFlightsTabActive()
     var self = this
@@ -56,38 +57,14 @@ class App extends Component {
       ;
   };
 
-// Method to render all flights:
+// METHOD TO DISPLAY ALL FLIGHTS
   renderAllFlights() {
     return this.state.data.map((flight, i) =>
     this.renderFlight(flight)
      )
   }
 
-  setDepartureTabActive(){
-    this.setState({
-      departuresActive: true,
-      arrivalsActive: false,
-      allFlightsActive: false
-    })
-  }
-
-  setArrivalsTabActive(){
-    this.setState({
-      departuresActive: false,
-      arrivalsActive: true,
-      allFlightsActive: false
-    })
-  }
-
-  setAllFlightsTabActive(){
-    this.setState({
-      departuresActive: false,
-      arrivalsActive: false,
-      allFlightsActive: true
-    })
-  }
-
-// Method to render all departures:
+// METHOD TO RENDER ALL DEPARTURES
 getDepartures() {
   this.setDepartureTabActive()
   var self = this
@@ -100,7 +77,16 @@ getDepartures() {
     ;
 };
 
-// Method to render all arrivals:
+// METHOD TO MARK ACTIVE TAB (DEPARTURES TAB)
+  setDepartureTabActive(){
+    this.setState({
+      departuresActive: true,
+      arrivalsActive: false,
+      allFlightsActive: false
+    })
+  }
+
+/// METHOD TO RENDER ALL ARRIVALS
 getArrivals() {
   this.setArrivalsTabActive()
   var self = this
@@ -113,16 +99,33 @@ getArrivals() {
     ;
 };
 
-// Method to search flight no:
+// METHOD TO MARK ACTIVE TAB (ARRIVALS TAB)
+  setArrivalsTabActive(){
+    this.setState({
+      departuresActive: false,
+      arrivalsActive: true,
+      allFlightsActive: false
+    })
+  }
 
+// METHOD TO MARK ACTIVE TAB (ALL FLIGHTS TAB)
+  setAllFlightsTabActive(){
+    this.setState({
+      departuresActive: false,
+      arrivalsActive: false,
+      allFlightsActive: true
+    })
+  }
+
+// METHOD TO TURN SEARCH BAR TEXT INPUT TO 'QUERY'
 handleInputChange(evt){
    this.setState({
      query: evt.target.value
    })
  }
 
+// METHOD TO UTILISE FLIGHT NO.'QUERY'. Part 1 (THIS PRELOADS ALL FLIGHT NO's)
  handleFlightNoSubmit(evt){
-
    var self = this
    evt.preventDefault()
    var fnArray = self.state.data.map((flight, i) =>
@@ -133,6 +136,7 @@ handleInputChange(evt){
   }
  }
 
+// METHOD TO UTILISE FLIGHT NO.'QUERY'. Part 2 (RETURNS FLIGHT AS AN OBJECT)
 searchFlightNo() {
   var self = this
   fetch("http://localhost:8000/flights/flight/" + this.state.query)
@@ -144,23 +148,20 @@ searchFlightNo() {
     };
 
 
-
-
-// Method to render a single flight
+// METHOD TO RENDER/DISPLAY A SINGLE FLIGHT
   renderFlight(flight) {
     return(
-
     <div className="wrapper">
-
       <div className="aside-1">
-        <img src={plane} style={{ height: "100%", width:"100%", borderRadius:"20px" }}/>
+          <img src={plane} style={{ height: "100%", width:"100%", borderRadius:"20px" }}/>
         </div>
         <div className="main">
           <p className="flight-flightno"> Flight No: {flight.FlightNo}                </p>
           <p className="flight-airline">  {flight.Airline} to {flight.PortOfCallA}    </p>
           <p className="flight-time">     Flight Time: {flight.Time} on {flight.Date} </p>
           <p className="flight-arrdep">   Arrival (or) Departure: {flight.ArrDep}     </p>
-          {/*<p className="flight-image">    Image: {flight.Image}           </p>*/}
+          {/*<p className="flight-image">    Image: {flight.Image}                    </p>*/}
+          {/*TODO: Figure how to render image from db as image, now string! */}
         </div>
         <div className="main">
           <p className="flight-status">   Status: {flight.Status}                     </p>
@@ -169,11 +170,11 @@ searchFlightNo() {
           <p className="flight-info2">    Further Info: {flight.Additional}           </p>
         </div>
       </div>
-  )
-}
+    )
+  }
 
 
-// Below is the UI:
+// -------------------  UI/RENDER SECTION (BELOW THIS LINE)   --------------------
   render() {
     if(this.state.data){
     return (
@@ -181,6 +182,7 @@ searchFlightNo() {
       <div className="homepage">
 
       {/*<img src={banner} style={{ height: "10%"}}/>*/}
+      {/*NB: Indecisive as to whether this 'banner' is required, a design decision  to be made */}
 
       <div className="pageheader">
         <ul>
@@ -200,12 +202,10 @@ searchFlightNo() {
             <h1>date / time</h1>
               <span class="foldable">
               <span>{this.state.curTime}</span>
-
             </span>
           </li>
         </ul>
         </div>
-
 
         <form className="searchform cf">
           <input
@@ -219,10 +219,6 @@ searchFlightNo() {
           </button>
         </form>
 
-
-
-
-
         <div class="dropdown-container">
           <input type="checkbox" id="drop" />
             <label for="drop">Arrivals / Departures</label>
@@ -233,23 +229,20 @@ searchFlightNo() {
               </ul>
         </div>
 
-
       <div >
         <p>{this.renderAllFlights()}</p>
       </div>
-
-
-      </div>
+    </div>
 
     )}
-
       else {
         return(
           <div>
             <p> Loading... </p>
           </div>
         )
-        }
-}}
+      }
+    }
+  }
 
 export default App;
